518d36b1a99434dd261e92ffd3ae1c6a2b8a48b2
Jose Martinez <josemanuel.martinez@cencosud.cl>
2023-10-31 09:14:19 -0300

    chore: updte cartId
    -Update cartId in redux when the cartId is changed from the url to test different ids

--- src/presentation/modules/cart/layouts/desktop/index.tsx
+++ src/presentation/modules/cart/layouts/desktop/index.tsx
@@ -4,16 +4,18 @@ import Desktop from '@components/layouts/desktop';
 import CartContainer from '@modules/cart/components/organisms/CartContainer';
 
 //Hooks
+import cartSlice from '@store/cart';
 import { useAppDispatch, useAppSelector } from '@hooks/storeHooks';
 import getCart from '@use-cases/cart/get-cart';
 import WindowsEvents from '@events/index';
 import useEventListener from '@hooks/eventListenerHooks';
 
 const CartDesktop = () => {
-  const dispatch = useAppDispatch();
-
+  const { addCartId } = cartSlice.actions;
   const { query } = useRouter();
   const { cartId } = useAppSelector((state) => state.cart);
+
+  const dispatch = useAppDispatch();
 
   const updateShippingCart = () => {
     if (cartId) {
@@ -28,8 +30,11 @@ const CartDesktop = () => {
   );
 
   useEffect(() => {
-    const cartId = query.cartId as string;
-    dispatch(getCart({ cartId }));
+    const queryCartId = query.cartId as string;
+    if (cartId === queryCartId) return;
+
+    dispatch(addCartId(queryCartId));
+    dispatch(getCart({ cartId: queryCartId }));
   }, []);
 
   return (
