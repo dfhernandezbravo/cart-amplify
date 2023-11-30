cf6db7db07d97c0f61b45a6f6f2e97796dc991c5
Jose Martinez <josemanuel.martinez@cencosud.cl>
2023-11-30 18:32:41 -0300

    fix: go to cart-page link

--- src/presentation/components/molecules/GoToCartButton/index.tsx
+++ src/presentation/components/molecules/GoToCartButton/index.tsx
@@ -4,12 +4,12 @@ import Link from 'next/link';
 import { enviroments } from '../../../../configs/env';
 
 const GoToCartButton = () => {
-  const { cartId, isHeadless } = useAppSelector((state) => state.cart);
+  const { cartBFF, isHeadless } = useAppSelector((state) => state.cart);
 
   return (
     <>
       <Link
-        href={`${enviroments.hostDomain}cart/${cartId}`}
+        href={`${enviroments.hostDomain}cart/${cartBFF?.id}`}
         target={isHeadless ? '_self' : '_parent'}
       >
         <Button className="linkBtn goToCartBtn fullWidth">
