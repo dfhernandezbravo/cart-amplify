59c30626316824200df5558a57dbfd46b88fab90
Carlos Pitre <carlospitre@Carloss-Mac-mini.local>
2024-01-18 19:01:51 -0500

    feat: wrapper events container

--- src/presentation/modules/cartAside/components/organisms/CartAsideContainer/index.tsx
+++ src/presentation/modules/cartAside/components/organisms/CartAsideContainer/index.tsx
@@ -4,25 +4,18 @@ import Cookies, { CookieAttributes } from 'js-cookie';
 import { SwipeableDrawer } from '@mui/material';
 import { selectTotalProductsInCart } from '@store/cart';
 import cartSlice from '@store/cart';
-import { setError } from '@store/error';
-import useEventListener from '@hooks/eventListenerHooks';
 import { useAppDispatch, useAppSelector } from '@hooks/storeHooks';
 import Header from '@modules/cartAside/sections/header';
 import Body from '@modules/cartAside/sections/body';
 import EmptyBody from '@modules/cartAside/sections/emptyBody';
 import Footer from '@modules/cartAside/sections/footer';
-import WindowsEvents from '@events/index';
-import handleHttpError from '@use-cases/error/handle-http-errors';
-import handlePayloadError from '@use-cases/error/handle-payload-errors';
 import getCart from '@use-cases/cart/get-cart';
 import updateItem from '@use-cases/cart/update-item';
 import addItem from '@use-cases/cart/add-item';
 import HybridationEvents from '../../../../../hybridationEvents';
 import getParamData from '@use-cases/cms/getParamData';
-import { CartAction } from '@entities/error/error.entity';
 import { getCartFromLocalStorage } from '@utils/getCartFromLocalStorage';
-import { customDispatchEvent } from '@store/events/dispatchEvents';
-import { Cart } from '@entities/cart/cart.entity';
+import WrapperEvents from './wrapper-events';
 
 const CartAsideContainer = () => {
   // hooks
@@ -31,103 +24,8 @@ const CartAsideContainer = () => {
   const dispatch = useAppDispatch();
 
   const totalProducts = useAppSelector(selectTotalProductsInCart);
-  const {
-    addCartId,
-    addProductInCart,
-    simulateAddProduct,
-    setIsHeadless,
-    simulateAddProductHeadless,
-    simulateRemoveProduct,
-    setCartAsideIsOpen,
-    setCart,
-  } = cartSlice.actions;
-
-  const handleSetIsOpen = (event: Event) => {
-    event.preventDefault();
-    const customEvent = event as CustomEvent;
-    dispatch(setCartAsideIsOpen(customEvent.detail?.open));
-  };
-
-  const handleAddProductEvent = (event: Event) => {
-    event.preventDefault();
-    const customEvent = event as CustomEvent;
-    dispatch(addProductInCart(customEvent.detail?.data));
-    // const customEventError = customEvent.detail?.data?.messagesErrors;
-
-    // if (customEventError.length) {
-    //   const cartError = handlePayloadError(customEventError[0], CartAction.ADD);
-    //   dispatch(setError(cartError));
-    // }
-    dispatch(setCartAsideIsOpen(true));
-  };
-
-  const handleGetCartId = (event: Event) => {
-    event.preventDefault();
-    const customEvent = event as CustomEvent;
-    dispatch(addCartId(customEvent.detail?.cartId));
-  };
-
-  const handleSimulateAddProductEvent = (event: Event) => {
-    event.preventDefault();
-    const customEvent = event as CustomEvent;
-    dispatch(simulateAddProductHeadless(customEvent.detail?.product));
-    dispatch(setCartAsideIsOpen(true));
-  };
-
-  const handleAddProductErrorEvent = (event: Event) => {
-    event.preventDefault();
-    const customEvent = event as CustomEvent;
-    const customEventError = customEvent.detail?.data.error;
-    const cartError = handleHttpError(customEventError, CartAction.ADD);
-    dispatch(setError(cartError));
-    setTimeout(() => {
-      dispatch(simulateRemoveProduct(customEvent.detail?.data?.itemId));
-    }, 4000);
-  };
-
-  const handleGetShoppingCart = (event: Event) => {
-    event.stopPropagation();
-    const customEvent = event as CustomEvent<{ shoppingCart: Cart }>;
-    const {
-      detail: { shoppingCart },
-    } = customEvent;
-    dispatch(setCart(shoppingCart));
-  };
-
-  // methods
-  const methods = {
-    initialize: () => {
-      if (typeof window !== 'undefined') {
-        useEventListener(
-          document,
-          WindowsEvents.TOGGLE_CART_ASIDE,
-          handleSetIsOpen,
-        );
-        useEventListener(
-          document,
-          WindowsEvents.ADD_PRODUCT_IN_CART,
-          handleAddProductEvent,
-        );
-        useEventListener(document, WindowsEvents.GET_CART_ID, handleGetCartId);
-        useEventListener(
-          document,
-          WindowsEvents.GET_SHOPPING_CART,
-          handleGetShoppingCart,
-        );
-        useEventListener(
-          document,
-          WindowsEvents.SIMULATE_ADD_PRODUCT,
-          handleSimulateAddProductEvent,
-        );
-        useEventListener(
-          document,
-          WindowsEvents.ADD_PRODUCT_ERROR,
-          handleAddProductErrorEvent,
-        );
-      }
-    },
-  };
-  methods.initialize();
+  const { addCartId, simulateAddProduct, setIsHeadless, setCartAsideIsOpen } =
+    cartSlice.actions;
 
   const setSalesChannelCookie = (
     cartId: string,
@@ -260,14 +158,6 @@ const CartAsideContainer = () => {
   }, [addCartId, cartBFF, cartId, dispatch]);
 
   useEffect(() => {
-    customDispatchEvent({
-      name: WindowsEvents.DISPATCH_GET_CART_ID,
-      detail: {},
-    });
-    customDispatchEvent({ name: WindowsEvents.DISPATCH_GET_CART, detail: {} });
-  }, []);
-
-  useEffect(() => {
     hybridation();
     return () => {
       window.removeEventListener('message', hybridation);
@@ -322,14 +212,16 @@ const CartAsideContainer = () => {
           }}
         >
           <Header />
-          {totalProducts > 0 ? (
-            <>
-              <Body />
-              <Footer />
-            </>
-          ) : (
-            <EmptyBody />
-          )}
+          <WrapperEvents>
+            {totalProducts > 0 ? (
+              <>
+                <Body />
+                <Footer />
+              </>
+            ) : (
+              <EmptyBody />
+            )}
+          </WrapperEvents>
         </SwipeableDrawer>
       )}
     </>
---
+++ src/presentation/modules/cartAside/components/organisms/CartAsideContainer/wrapper-events.tsx
@@ -0,0 +1,140 @@
+import { Cart } from '@entities/cart/cart.entity';
+import { CartAction } from '@entities/error/error.entity';
+import WindowsEvents from '@events/index';
+import { useAppDispatch } from '@hooks/storeHooks';
+import cartSlice from '@store/cart';
+import { setError } from '@store/error';
+import { customDispatchEvent } from '@store/events/dispatchEvents';
+import handleHttpError from '@use-cases/error/handle-http-errors';
+import React, { useEffect } from 'react';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const WrapperEvents: React.FC<Props> = ({ children }) => {
+  const {
+    addCartId,
+    addProductInCart,
+    simulateAddProductHeadless,
+    simulateRemoveProduct,
+    setCartAsideIsOpen,
+    setCart,
+  } = cartSlice.actions;
+
+  const dispatch = useAppDispatch();
+
+  const handleSetIsOpen = (event: Event) => {
+    event.stopImmediatePropagation();
+    const customEvent = event as CustomEvent;
+    dispatch(setCartAsideIsOpen(customEvent.detail?.open));
+  };
+
+  const handleGetCartId = (event: Event) => {
+    event.preventDefault();
+    const customEvent = event as CustomEvent<{ cartId: string }>;
+    const {
+      detail: { cartId },
+    } = customEvent;
+    dispatch(addCartId(cartId));
+  };
+
+  const handleSimulateAddProductEvent = (event: Event) => {
+    event.preventDefault();
+    const customEvent = event as CustomEvent;
+    dispatch(simulateAddProductHeadless(customEvent.detail?.product));
+    dispatch(setCartAsideIsOpen(true));
+  };
+
+  const handleGetShoppingCart = (event: Event) => {
+    event.preventDefault();
+    const customEvent = event as CustomEvent<{ shoppingCart: Cart }>;
+    const {
+      detail: { shoppingCart },
+    } = customEvent;
+    dispatch(setCart(shoppingCart));
+  };
+
+  const handleAddProductErrorEvent = (event: Event) => {
+    event.preventDefault();
+    const customEvent = event as CustomEvent;
+    const customEventError = customEvent.detail?.data.error;
+    const cartError = handleHttpError(customEventError, CartAction.ADD);
+    dispatch(setError(cartError));
+    setTimeout(() => {
+      dispatch(simulateRemoveProduct(customEvent.detail?.data?.itemId));
+    }, 4000);
+  };
+
+  const handleAddProductEvent = (event: Event) => {
+    event.preventDefault();
+    const customEvent = event as CustomEvent;
+    dispatch(addProductInCart(customEvent.detail?.data));
+    // const customEventError = customEvent.detail?.data?.messagesErrors;
+
+    // if (customEventError.length) {
+    //   const cartError = handlePayloadError(customEventError[0], CartAction.ADD);
+    //   dispatch(setError(cartError));
+    // }
+    dispatch(setCartAsideIsOpen(true));
+  };
+
+  useEffect(() => {
+    document.addEventListener(WindowsEvents.TOGGLE_CART_ASIDE, handleSetIsOpen);
+    document.addEventListener(
+      WindowsEvents.SIMULATE_ADD_PRODUCT,
+      handleSimulateAddProductEvent,
+    );
+
+    window.addEventListener(WindowsEvents.GET_CART_ID, handleGetCartId);
+    window.addEventListener('GET_SHOPPING_CART', handleGetShoppingCart);
+    window.addEventListener(
+      WindowsEvents.ADD_PRODUCT_ERROR,
+      handleAddProductErrorEvent,
+    );
+    window.addEventListener(
+      WindowsEvents.ADD_PRODUCT_IN_CART,
+      handleAddProductEvent,
+    );
+
+    return () => {
+      document.removeEventListener(
+        WindowsEvents.SIMULATE_ADD_PRODUCT,
+        handleSimulateAddProductEvent,
+      );
+
+      window.removeEventListener(WindowsEvents.GET_CART_ID, handleGetCartId);
+      window.removeEventListener('GET_SHOPPING_CART', handleGetShoppingCart);
+
+      window.removeEventListener(
+        WindowsEvents.ADD_PRODUCT_ERROR,
+        handleAddProductErrorEvent,
+      );
+      window.removeEventListener(
+        WindowsEvents.ADD_PRODUCT_IN_CART,
+        handleAddProductEvent,
+      );
+    };
+  }, [
+    handleSimulateAddProductEvent,
+    handleGetCartId,
+    handleAddProductEvent,
+    handleAddProductErrorEvent,
+    handleGetShoppingCart,
+  ]);
+
+  useEffect(() => {
+    customDispatchEvent({
+      name: WindowsEvents.DISPATCH_GET_CART_ID,
+      detail: {},
+    });
+    customDispatchEvent({
+      name: WindowsEvents.DISPATCH_GET_CART,
+      detail: {},
+    });
+  }, []);
+
+  return <>{children}</>;
+};
+
+export default WrapperEvents;
