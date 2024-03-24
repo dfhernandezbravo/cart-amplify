b52e03b388bb51c33ed20150256826d85f9577a5
Jose Martinez <josemanuel.martinez@cencosud.cl>
2024-03-24 20:22:07 -0300

    chore: adding channel cookie

--- src/presentation/modules/cartAside/index.tsx
+++ src/presentation/modules/cartAside/index.tsx
@@ -1,12 +1,15 @@
 import CartAsideDesktop from '@modules/cartAside/layouts/desktop';
 import CartAsideMobile from '@modules/cartAside/layouts/mobile';
 import MainLayout from '@components/layouts/provider/main-layout';
+import CartAsideProvider from './layouts/provider';
 
 const CartAside = () => {
   return (
     <MainLayout>
-      <CartAsideMobile />
-      <CartAsideDesktop />
+      <CartAsideProvider>
+        <CartAsideMobile />
+        <CartAsideDesktop />
+      </CartAsideProvider>
     </MainLayout>
   );
 };
