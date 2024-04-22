413480200c128096ea431b200ab71de6d2832fb4
Luis <luisivan.castillovillafuerte@externos-cl.cencosud.com>
2024-04-22 12:19:59 -0500

    feat: add UPDATE_SHIPPING_CART event in cartAside

--- src/presentation/modules/cartAside/layouts/provider/index.tsx
+++ src/presentation/modules/cartAside/layouts/provider/index.tsx
@@ -1,20 +1,36 @@
 import { useEffect } from 'react';
 import Cookies from 'js-cookie';
 import { COOKIES } from '@entities/cart/cookies';
-import { useAppSelector } from '@hooks/storeHooks';
+import { useAppDispatch, useAppSelector } from '@hooks/storeHooks';
+import useEventListener from '@hooks/eventListenerHooks';
+import WindowsEvents from '@events/index';
+import getCart from '@use-cases/cart/get-cart';
 
 interface Props {
   children: React.ReactNode;
 }
 
 const CartAsideProvider = ({ children }: Props) => {
-  const { cartBFF } = useAppSelector((state) => state.cart);
+  const { cartBFF, cartId } = useAppSelector((state) => state.cart);
   const channel = cartBFF?.channel;
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
     if (!channel) return;
     Cookies.set(COOKIES.CHANNEL, channel);
   });
+
+  const updateShippingCart = () => {
+    if (cartId) {
+      dispatch(getCart({ cartId }));
+    }
+  };
+
+  useEventListener(
+    document,
+    WindowsEvents.UPDATE_SHIPPING_CART,
+    updateShippingCart,
+  );
 
   return <>{children}</>;
 };
